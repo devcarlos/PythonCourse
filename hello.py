7457d9e2656fb8a8400b10b6b90b677bed944bd0
Carlos Alcala <carlos.alcala@me.com>
2020-05-06 09:02:10 -0400

    Hello Python

---
+++ hello.py
@@ -0,0 +1,28 @@
+import string
+
+msg = "Hello World !"
+print(msg)
+
+msg2 = msg.upper()
+print(msg2)
+
+pi = 3.1416
+
+print(pi)
+
+num1 = 10
+num2 = 5
+
+print(num1 + num2)
+
+print(num1 * num2)
+
+days = 28
+
+print(str(days) + ' days')
+
+number1 = input('Enter 1st number: ')
+number2 = input('Enter 2nd number: ')
+
+print(int(number1) + int(number2))
+print(float(number1) + float(number2))
